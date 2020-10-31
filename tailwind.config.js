aa0cfcc87d9528bfa82ef75a96f6eacc0568d291
benali djamel <eeeee0566@gmail.com>
2020-10-31 10:08:10 +0100

    add login page

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'gray-light-form': '#BEBEBE',
+        'blue-base' : '#7174E2',
+        'body-color': '#F3F3F3'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
